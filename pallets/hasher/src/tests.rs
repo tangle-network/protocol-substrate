a44b27ad1633d187b3b2011ea57204944377385e
Shady Khalifa <shekohex@gmail.com>
2021-08-09 19:40:09 +0200

    add hasher tests

--- pallets/hasher/src/tests.rs
+++ pallets/hasher/src/tests.rs
@@ -1,14 +1,44 @@
 use super::*;
 use crate::mock::*;
-use frame_support::{assert_err, instances::Instance1};
+use arkworks_gadgets::poseidon::PoseidonParameters;
+use frame_support::{assert_err, assert_ok, instances::Instance1};
+use sp_core::bytes;
 
 #[test]
-fn hash_nothing_with_test_hasher() {
+fn should_fail_with_params_not_initialized() {
 	new_test_ext().execute_with(|| {
-		// Read pallet storage and assert an expected result.
 		assert_err!(
-			<HasherPallet as HasherModule>::hash(&[1u8; 32]),
+			<BN254Poseidon3x5Hasher as HasherModule>::hash(&[1u8; 32]),
 			Error::<Test, Instance1>::ParametersNotInitialized
+		);
+	});
+}
+
+#[test]
+fn should_initialize_parameters() {
+	new_test_ext().execute_with(|| {
+		let params = PoseidonParameters::<ark_bn254::Fr>::new(
+			constants::bn256_3x5::ROUND_KEYS.into(),
+			constants::bn256_3x5::MDS_MATRIX.iter().map(|v| v.to_vec()).collect(),
+		);
+		let res = BN254Poseidon3x5Hasher::force_set_parameters(Origin::root(), params.to_bytes());
+		assert_ok!(res);
+	});
+}
+
+#[test]
+fn should_output_correct_hash() {
+	new_test_ext().execute_with(|| {
+		let params = PoseidonParameters::new(
+			constants::bn256_3x5::ROUND_KEYS.into(),
+			constants::bn256_3x5::MDS_MATRIX.iter().map(|v| v.to_vec()).collect(),
+		);
+		let res = BN254Poseidon3x5Hasher::force_set_parameters(Origin::root(), params.to_bytes());
+		assert_ok!(res);
+		let hash = BN254Poseidon3x5Hasher::hash_two(&[1u8; 32], &[2u8; 32]);
+		assert_ok!(
+			hash,
+			bytes::from_hex("0x1acd1ec9914d2b378db637af233324a068ea40e6f26ff34e191bd77816e9810c").unwrap()
 		);
 	});
 }
--- primitives/src/hashing/arkworks.rs
+++ primitives/src/hashing/arkworks.rs
@@ -1,22 +1,25 @@
 use crate::*;
-use ark_crypto_primitives::{Error, CRH as CRHTrait};
+use ark_crypto_primitives::{
+	crh::poseidon::{PoseidonRoundParams, CRH as PoseidonSpongeCRH},
+	Error, CRH as CRHTrait,
+};
 use ark_ff::{BigInteger, PrimeField};
 use arkworks_gadgets::poseidon::{sbox::PoseidonSbox, PoseidonParameters, Rounds, CRH};
 use sp_std::{marker::PhantomData, vec::Vec};
 
-#[derive(Default, Clone)]
-pub struct PoseidonRounds3;
-#[derive(Default, Clone)]
-pub struct PoseidonRounds5;
+#[derive(Default, Clone, Copy)]
+pub struct PoseidonRounds3x5;
+#[derive(Default, Clone, Copy)]
+pub struct PoseidonRounds5x5;
 
-impl Rounds for PoseidonRounds3 {
+impl Rounds for PoseidonRounds3x5 {
 	const FULL_ROUNDS: usize = 8;
 	const PARTIAL_ROUNDS: usize = 57;
 	const SBOX: PoseidonSbox = PoseidonSbox::Exponentiation(5);
 	const WIDTH: usize = 3;
 }
 
-impl Rounds for PoseidonRounds5 {
+impl Rounds for PoseidonRounds5x5 {
 	const FULL_ROUNDS: usize = 8;
 	const PARTIAL_ROUNDS: usize = 60;
 	const SBOX: PoseidonSbox = PoseidonSbox::Exponentiation(5);
@@ -29,10 +32,25 @@ impl<F: PrimeField, P: Rounds> InstanceHasher for ArkworksPoseidonHasher<F, P> {
 	fn hash(input: &[u8], param_bytes: &[u8]) -> Result<Vec<u8>, Error> {
 		let params = PoseidonParameters::<F>::from_bytes(param_bytes)?;
 		let output: F = <CRH<F, P> as CRHTrait>::evaluate(&params, input)?;
-		Ok(output.into_repr().to_bytes_le())
+		// we use big-endian because it the same for
+		// solidity contracts and javascript circom implementations.
+		let value = output.into_repr().to_bytes_be();
+		Ok(value)
+	}
+}
+
+pub struct ArkworksPoseidonSpongeHasher<F: PrimeField, P: PoseidonRoundParams<F>>(PhantomData<F>, PhantomData<P>);
+
+impl<F: PrimeField, P: PoseidonRoundParams<F>> InstanceHasher for ArkworksPoseidonSpongeHasher<F, P> {
+	fn hash(input: &[u8], param_bytes: &[u8]) -> Result<Vec<u8>, Error> {
+		todo!("parse params bytes into PoseidonRoundParams");
 	}
 }
 
 use ark_bls12_381::Fr as Bls381;
-pub type BLS381Poseidon3Rounds = ArkworksPoseidonHasher<Bls381, PoseidonRounds3>;
-pub type BLS381Poseidon5Rounds = ArkworksPoseidonHasher<Bls381, PoseidonRounds5>;
+pub type BLS381Poseidon3x5Hasher = ArkworksPoseidonHasher<Bls381, PoseidonRounds3x5>;
+pub type BLS381Poseidon5x5Hasher = ArkworksPoseidonHasher<Bls381, PoseidonRounds5x5>;
+
+use ark_bn254::Fr as Bn254;
+pub type BN254Poseidon3x5Hasher = ArkworksPoseidonHasher<Bn254, PoseidonRounds3x5>;
+pub type BN254Poseidon5x5Hasher = ArkworksPoseidonHasher<Bn254, PoseidonRounds5x5>;
