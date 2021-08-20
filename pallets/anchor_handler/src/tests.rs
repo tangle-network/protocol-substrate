24b23e138763dfe2186724755228dc6c1fab5e81
Gaurav Tiwari <gaurav1@tutamail.com>
2021-08-20 10:36:10 +0530

    Add anchor_handler pallet
    Import chainbridge substrate pallet

---
+++ pallets/anchor_handler/src/tests.rs
@@ -0,0 +1,13 @@
+use crate::{mock::*, Error};
+use frame_support::assert_err;
+
+#[test]
+fn set_maintainer() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_err!(
+			AnchorHandler::set_maintainer(Origin::signed(1), 42),
+			Error::<Test>::InvalidPermissions
+		);
+	});
+}
