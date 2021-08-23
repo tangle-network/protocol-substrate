05d782dab8095487f872f94ca7f0211540fcdf0c
Shady Khalifa <shekohex@gmail.com>
2021-08-23 20:24:19 +0200

    first test works!

--- pallets/mixer/src/tests.rs
+++ pallets/mixer/src/tests.rs
@@ -1,6 +1,25 @@
+use arkworks_gadgets::{
+	poseidon::PoseidonParameters,
+	utils::{get_mds_poseidon_circom_bn254_x5_3, get_rounds_poseidon_circom_bn254_x5_3},
+};
+use frame_support::{assert_ok, traits::OnInitialize};
+use sp_runtime::traits::One;
+
 use crate::mock::*;
 
+fn hasher_params() -> Vec<u8> {
+	let rounds = get_rounds_poseidon_circom_bn254_x5_3::<ark_bn254::Fr>();
+	let mds = get_mds_poseidon_circom_bn254_x5_3::<ark_bn254::Fr>();
+	let params = PoseidonParameters::new(rounds, mds);
+	params.to_bytes()
+}
+
 #[test]
-fn hash_nothing_with_test_hasher() {
-	new_test_ext().execute_with(|| {});
+fn shout_create_new_mixer() {
+	new_test_ext().execute_with(|| {
+		// init hasher pallet first.
+		assert_ok!(HasherPallet::force_set_parameters(Origin::root(), hasher_params()));
+		<MerkleTree as OnInitialize<u64>>::on_initialize(1);
+		assert_ok!(Mixer::create(Origin::root(), One::one(), 3));
+	});
 }
