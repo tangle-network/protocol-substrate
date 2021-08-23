9d240538eb6c5176bf1b6d0a402c8ad55e32eddb
Gaurav Tiwari <gaurav1@tutamail.com>
2021-08-23 09:35:13 +0530

    Bridge pallet Frame V2 migration done
    all original test-cases are passing

--- pallets/bridge/src/types.rs
+++ pallets/bridge/src/types.rs
@@ -1,9 +1,9 @@
 use sp_runtime::RuntimeDebug;
 use sp_std::prelude::*;
 
-use codec::{Decode, Encode, EncodeLike};
+use codec::{Decode, Encode};
 
-const DEFAULT_RELAYER_THRESHOLD: u32 = 1;
+pub const DEFAULT_RELAYER_THRESHOLD: u32 = 1;
 
 pub type ChainId = u8;
 pub type DepositNonce = u64;
@@ -24,13 +24,10 @@ pub struct ProposalVotes<AccountId, BlockNumber> {
 	pub expiry: BlockNumber,
 }
 
-// impl<AccountId, BlockNumber> EncodeLike for ProposalVotes<AccountId,
-// BlockNumber> {}
-
 impl<A: PartialEq, B: PartialOrd + Default> ProposalVotes<A, B> {
 	/// Attempts to mark the proposal as approve or rejected.
 	/// Returns true if the status changes from active.
-	pub fn try_to_complete(&mut self, threshold: u32, total: u32) -> ProposalStatus {
+	pub(crate) fn try_to_complete(&mut self, threshold: u32, total: u32) -> ProposalStatus {
 		if self.votes_for.len() >= threshold as usize {
 			self.status = ProposalStatus::Approved;
 			ProposalStatus::Approved
@@ -44,17 +41,17 @@ impl<A: PartialEq, B: PartialOrd + Default> ProposalVotes<A, B> {
 
 	/// Returns true if the proposal has been rejected or approved, otherwise
 	/// false.
-	pub fn is_complete(&self) -> bool {
+	pub(crate) fn is_complete(&self) -> bool {
 		self.status != ProposalStatus::Initiated
 	}
 
 	/// Returns true if `who` has voted for or against the proposal
-	pub fn has_voted(&self, who: &A) -> bool {
+	pub(crate) fn has_voted(&self, who: &A) -> bool {
 		self.votes_for.contains(&who) || self.votes_against.contains(&who)
 	}
 
 	/// Return true if the expiry time has been reached
-	pub fn is_expired(&self, now: B) -> bool {
+	pub(crate) fn is_expired(&self, now: B) -> bool {
 		self.expiry <= now
 	}
 }
