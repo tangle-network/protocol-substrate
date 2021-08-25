5bc88e3a0510f1e85f00f5452349a902d009c334
Gaurav Tiwari <gaurav1@tutamail.com>
2021-08-25 12:09:25 +0530

    Use bridge-pallet in anchor-handler
    Make anchor-handler non-instantiable

--- pallets/anchor-handler/src/mock.rs
+++ pallets/anchor-handler/src/mock.rs
@@ -1,11 +1,12 @@
 use super::*;
 use crate as pallet_anchor_handler;
-use frame_support::parameter_types;
+use frame_support::{ord_parameter_types, parameter_types, PalletId};
 use frame_system as system;
+pub use pallet_balances;
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
-	traits::{BlakeTwo256, IdentityLookup},
+	traits::{AccountIdConversion, BlakeTwo256, IdentityLookup},
 };
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
@@ -19,6 +20,8 @@ frame_support::construct_runtime!(
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
 		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		Balances: pallet_balances::{Pallet, Call, Storage, Event<T>},
+		Bridge: pallet_bridge::{Pallet, Call, Storage, Event<T>},
 		AnchorHandler: pallet_anchor_handler::{Pallet, Call, Storage, Event<T>},
 	}
 );
@@ -29,7 +32,7 @@ parameter_types! {
 }
 
 impl system::Config for Test {
-	type AccountData = ();
+	type AccountData = pallet_balances::AccountData<u64>;
 	type AccountId = u64;
 	type BaseCallFilter = ();
 	type BlockHashCount = BlockHashCount;
@@ -54,12 +57,105 @@ impl system::Config for Test {
 	type Version = ();
 }
 
-impl pallet_anchor_handler::Config for Test {
-	type Event = Event;
-	type ForceOrigin = frame_system::EnsureRoot<u64>;
+parameter_types! {
+	pub const ExistentialDeposit: u64 = 1;
 }
 
-// Build genesis storage according to the mock runtime.
+ord_parameter_types! {
+	pub const One: u64 = 1;
+}
+
+impl pallet_balances::Config for Test {
+	type AccountStore = System;
+	type Balance = u64;
+	type DustRemoval = ();
+	type Event = Event;
+	type ExistentialDeposit = ExistentialDeposit;
+	type MaxLocks = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const ChainIdentity: u8 = 5;
+	pub const ProposalLifetime: u64 = 50;
+	pub const BridgeAccountId: PalletId = PalletId(*b"dw/bridg");
+}
+
+impl pallet_bridge::Config for Test {
+	type AdminOrigin = frame_system::EnsureRoot<Self::AccountId>;
+	type BridgeAccountId = BridgeAccountId;
+	type ChainIdentity = ChainIdentity;
+	type Event = Event;
+	type Proposal = Call;
+	type ProposalLifetime = ProposalLifetime;
+}
+
+impl pallet_anchor_handler::Config for Test {
+	type BridgeOrigin = pallet_bridge::EnsureBridge<Test>;
+	type Event = Event;
+}
+
+pub const RELAYER_A: u64 = 0x2;
+pub const RELAYER_B: u64 = 0x3;
+pub const RELAYER_C: u64 = 0x4;
+pub const ENDOWED_BALANCE: u64 = 100_000_000;
+
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+	let bridge_id = PalletId(*b"dw/bridg").into_account();
+	let mut t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+	pallet_balances::GenesisConfig::<Test> {
+		balances: vec![(bridge_id, ENDOWED_BALANCE), (RELAYER_A, ENDOWED_BALANCE)],
+	}
+	.assimilate_storage(&mut t)
+	.unwrap();
+	let mut ext = sp_io::TestExternalities::new(t);
+	ext.execute_with(|| System::set_block_number(1));
+	ext
+}
+
+fn last_event() -> Event {
+	system::Pallet::<Test>::events()
+		.pop()
+		.map(|e| e.event)
+		.expect("Event expected")
+}
+
+pub fn expect_event<E: Into<Event>>(e: E) {
+	assert_eq!(last_event(), e.into());
+}
+
+// Asserts that the event was emitted at some point.
+pub fn event_exists<E: Into<Event>>(e: E) {
+	let actual: Vec<Event> = system::Pallet::<Test>::events()
+		.iter()
+		.map(|e| e.event.clone())
+		.collect();
+	let e: Event = e.into();
+	let mut exists = false;
+	for evt in actual {
+		if evt == e {
+			exists = true;
+			break;
+		}
+	}
+	assert!(exists);
+}
+
+// Checks events against the latest. A contiguous set of events must be
+// provided. They must include the most recent event, but do not have to include
+// every past event.
+pub fn assert_events(mut expected: Vec<Event>) {
+	let mut actual: Vec<Event> = system::Pallet::<Test>::events()
+		.iter()
+		.map(|e| e.event.clone())
+		.collect();
+
+	expected.reverse();
+
+	for evt in expected {
+		let next = actual.pop().expect("event expected");
+		assert_eq!(next, evt.into(), "Events don't match");
+	}
 }
