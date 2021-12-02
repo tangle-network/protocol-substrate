f5adc0ac42cc7a99fe967881f88f90ac6918ba8e
mfaghihi <mfaghihi.h@gmail.com>
2021-12-02 14:33:11 -0500

    fixes anchor tests failing

--- primitives/src/verifying/arkworks.rs
+++ primitives/src/verifying/arkworks.rs
@@ -1,10 +1,9 @@
 use crate::*;
-use ark_crypto_primitives::Error;
+use ark_crypto_primitives::{Error, commitment};
 use ark_ec::PairingEngine;
 use ark_ff::Zero;
 use ark_groth16::{Proof, VerifyingKey};
 use ark_serialize::CanonicalDeserialize;
-use arkworks_circuits::setup::{bridge, mixer};
 use arkworks_utils::utils::{common::verify_groth16, to_field_elements};
 use sp_std::marker::PhantomData;
 
@@ -14,17 +13,9 @@ pub struct ArkworksBridgeVerifierGroth16<E: PairingEngine>(PhantomData<E>);
 impl<E: PairingEngine> InstanceVerifier for ArkworksMixerVerifierGroth16<E> {
 	fn verify(public_inp_bytes: &[u8], proof_bytes: &[u8], vk_bytes: &[u8]) -> Result<bool, Error> {
 		let public_input_field_elts = to_field_elements::<E::Fr>(public_inp_bytes).unwrap();
-		let public_inputs = mixer::get_public_inputs::<E::Fr>(
-			public_input_field_elts[0], // nullifier_hash
-			public_input_field_elts[1], // root
-			public_input_field_elts[2], // recipient
-			public_input_field_elts[3], // relayer
-			public_input_field_elts[4], // fee
-			public_input_field_elts[5], // refund
-		);
 		let vk = VerifyingKey::<E>::deserialize(vk_bytes)?;
 		let proof = Proof::<E>::deserialize(proof_bytes)?;
-		let res = verify_groth16::<E>(&vk, &public_inputs, &proof);
+		let res = verify_groth16::<E>(&vk, &public_input_field_elts, &proof);
 		Ok(res)
 	}
 }
@@ -43,10 +34,10 @@ impl<E: PairingEngine> InstanceVerifier for ArkworksBridgeVerifierGroth16<E> {
 		const M: usize = 2;
 		let mut roots = [E::Fr::zero(); M];
 		for (i, root) in roots.iter_mut().enumerate() {
-			*root = *public_input_field_elts.get(i + 6).unwrap_or(&E::Fr::zero());
+			*root = *public_input_field_elts.get(i + 7).unwrap_or(&E::Fr::zero());
 		}
 
-		let public_inputs = bridge::get_public_inputs::<E::Fr, M>(
+		let public_inputs = get_public_inputs::<E::Fr, M>(
 			chain_id,
 			nullifier_hash,
 			roots,
@@ -62,6 +53,33 @@ impl<E: PairingEngine> InstanceVerifier for ArkworksBridgeVerifierGroth16<E> {
 		let res = verify_groth16::<E>(&vk, &public_inputs, &proof);
 		Ok(res)
 	}
+}
+
+use ark_ff::PrimeField;
+#[allow(clippy::too_many_arguments)]
+pub fn get_public_inputs<F: PrimeField, const M: usize>(
+	chain_id: F,
+	nullifier_hash: F,
+	roots: [F; M],
+	root: F,
+	recipient: F,
+	relayer: F,
+	fee: F,
+	refund: F,
+	commitment: F,
+) -> Vec<F> {
+	vec![
+		chain_id,
+		nullifier_hash,
+		roots[M - 2],
+		roots[M - 1],
+		root,
+		recipient,
+		relayer,
+		fee,
+		refund,
+		commitment,
+	]
 }
 
 use ark_bls12_381::Bls12_381;
