2bd9440aaa4e2ddaa843c09f5aeca7ffbf083961
Akilesh Tangella <saiakilesh@gmail.com>
2022-01-09 12:11:01 -0500

    some progress on mock.rs

--- pallets/token-wrapper/src/lib.rs
+++ pallets/token-wrapper/src/lib.rs
@@ -152,17 +152,17 @@ pub mod pallet {
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		// #[pallet::weight(<T as Config>::WeightInfo::set_wrapping_fee())]
-		// pub fn set_wrapping_fee(origin: OriginFor<T>, fee: BalanceOf<T>) -> DispatchResultWithPostInfo {
-		// 	ensure_root(origin)?;
+		#[pallet::weight(<T as Config>::WeightInfo::set_wrapping_fee())]
+		pub fn set_wrapping_fee(origin: OriginFor<T>, fee: BalanceOf<T>) -> DispatchResultWithPostInfo {
+			ensure_root(origin)?;
 			
-		// 	WrappingFeePercent::<T>::put(fee);
+			WrappingFeePercent::<T>::put(fee);
 
-		// 	Self::deposit_event(Event::UpdatedWrappingFeePercent {
-		// 		wrapping_fee_percent: fee,
-		// 	});
-		// 	Ok(().into())
-		// }
+			Self::deposit_event(Event::UpdatedWrappingFeePercent {
+				wrapping_fee_percent: fee,
+			});
+			Ok(().into())
+		}
 
 		#[pallet::weight(<T as Config>::WeightInfo::wrap())]
 		pub fn wrap(
