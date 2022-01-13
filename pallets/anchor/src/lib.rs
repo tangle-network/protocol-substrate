dc704512a339a49ac2a818decc866f7ac5e85445
drewstone <drewstone329@gmail.com>
2022-01-13 14:28:15 -0500

    Add refresh capability to anchor since it was missing (#95)
    * Add refresh capability to anchor
    
    * Update

--- pallets/anchor/src/lib.rs
+++ pallets/anchor/src/lib.rs
@@ -143,6 +143,8 @@ pub mod pallet {
 		AnchorCreation { tree_id: T::TreeId },
 		/// Amount has been withdrawn from the anchor
 		Withdraw { who: T::AccountId, amount: BalanceOf<T, I> },
+		/// A transaction has been refreshed (one spent, another inserted)
+		Refresh { tree_id: T::TreeId, leaf: T::Element },
 		/// Amount has been deposited into the anchor
 		Deposit {
 			depositor: T::AccountId,
@@ -365,13 +367,24 @@ impl<T: Config<I>, I: 'static> AnchorInterface<AnchorConfigration<T, I>> for Pal
 		bytes.extend_from_slice(&commitment.encode());
 		let result = <T as pallet::Config<I>>::Verifier::verify(&bytes, proof_bytes)?;
 		ensure!(result, Error::<T, I>::InvalidWithdrawProof);
-		// transfer the assets
+		// withdraw or refresh depending on the refresh commitment value
 		let anchor = Self::get_anchor(id)?;
-		<T as Config<I>>::Currency::transfer(anchor.asset, &Self::account_id(), &recipient, anchor.deposit_size)?;
-		Self::deposit_event(Event::Withdraw {
-			who: recipient.clone(),
-			amount: anchor.deposit_size,
-		});
+		if commitment.encode() == T::Element::default().encode() {
+			// transfer the deposit to the recipient when the commitment is default / zero (a withdrawal)
+			<T as Config<I>>::Currency::transfer(anchor.asset, &Self::account_id(), &recipient, anchor.deposit_size)?;
+			Self::deposit_event(Event::Withdraw {
+				who: recipient.clone(),
+				amount: anchor.deposit_size,
+			});
+		} else {
+			// deposit the new commitment when the commitment is not default / zero (a refresh)
+			T::LinkableTree::insert_in_order(id, commitment)?;
+			Self::deposit_event(Event::Refresh {
+				tree_id: id,
+				leaf: commitment,
+			});
+		}
+
 		Ok(())
 	}
 
