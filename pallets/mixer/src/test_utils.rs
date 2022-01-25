8e8d0c7a4ca87581a5ff1984661dbe9077d849c3
Filip Lazovic <lazoviccorp@gmail.com>
2022-01-25 17:30:00 +0100

    Testing wasm-utils

--- pallets/mixer/src/test_utils.rs
+++ pallets/mixer/src/test_utils.rs
@@ -1,8 +1,13 @@
 use ark_bn254::Bn254;
 use ark_ff::{BigInteger, FromBytes, PrimeField};
-use arkworks_circuits::setup::{common::{prove_unchecked, prove}, mixer::{MixerProverSetup, setup_leaf_x5_5, setup_proof_x5_5}};
+use arkworks_circuits::setup::{common::{prove_unchecked, prove, verify_unchecked_raw}, mixer::{MixerProverSetup, setup_leaf_x5_5, setup_proof_x5_5, setup_leaf_with_privates_raw_x5_5}};
 use arkworks_utils::utils::common::{Curve};
 use webb_primitives::ElementTrait;
+
+// wasm-utils dependencies
+use wasm_utils::proof::{generate_proof_js, ProofInputBuilder, ProofInput, MixerProofInput, JsProofInput};
+use wasm_utils::note::JsNote;
+use wasm_utils::types::{Curve as WasmCurve, Backend, Leaves};
 
 use crate::mock::Element;
 
@@ -41,6 +46,60 @@ pub fn setup_zk_circuit(
 			let root_element = Element::from_bytes(&root);
 
 			(proof, root_element, nullifier_hash_element, leaf_element)
+		}
+		Curve::Bls381 => {
+			unimplemented!()
+		}
+	}
+}
+
+pub fn setup_wasm_utils_zk_circuit(
+	curve: Curve,
+	recipient_bytes: Vec<u8>,
+	relayer_bytes: Vec<u8>,
+	pk_bytes: Vec<u8>,
+	fee_value: u128,
+	refund_value: u128,
+) -> (
+	Vec<u8>, // proof bytes
+	Element, // root
+	Element, // nullifier_hash
+	Element, // leaf
+) {
+	match curve {
+		Curve::Bn254 => {
+			let note_secret = "7e0f4bfa263d8b93854772c94851c04b3a9aba38ab808a8d081f6f5be9758110b7147c395ee9bf495734e4703b1f622009c81712520de0bbd5e7a10237c7d829bf6bd6d0729cca778ed9b6fb172bbb12b01927258aca7e0a66fd5691548f8717";
+
+			let secret = hex::decode(&note_secret[0..32]).unwrap();
+			let nullifier = hex::decode(&note_secret[32..64]).unwrap();
+			let (leaf, _) = setup_leaf_with_privates_raw_x5_5::<Bn254Fr>(curve, secret.clone(), nullifier.clone()).unwrap();
+			
+			let leaves = vec![leaf];
+
+			let mixer_proof_input = MixerProofInput {
+				exponentiation: 5,
+				width: 5,
+				curve: WasmCurve::Bn254,
+				backend: Backend::Arkworks,
+				secrets: secret,
+				nullifier,
+				recipient: recipient_bytes,
+				relayer: relayer_bytes,
+				pk: pk_bytes,
+				refund: refund_value,
+				fee: fee_value,
+				chain_id: 0,
+				leaves,
+				leaf_index: 0
+			};
+			let js_proof_inputs = JsProofInput { inner: ProofInput::Mixer(mixer_proof_input) };
+			let proof = generate_proof_js(js_proof_inputs).unwrap();
+
+			let root_element = Element::from_bytes(&proof.root);
+			let nullifier_hash_element = Element::from_bytes(&proof.nullifier_hash);
+			let leaf_element = Element::from_bytes(&proof.leaf);
+
+			(proof.proof, root_element, nullifier_hash_element, leaf_element)
 		}
 		Curve::Bls381 => {
 			unimplemented!()
