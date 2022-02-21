73ed31d705d1cf362109fa97c07ea77eb761ff37
Filip Lazovic <lazoviccorp@gmail.com>
2022-02-21 12:04:38 +0100

    Added CI script for integration tests

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-# get the path for this
+# get the path for this file
 BASEDIR=$(dirname "$0") 
 
-# release mode
-# should find client/Cargo.toml no matter where its called from
-# should run test in synchronous way to avoid race conditions
-# output logs -- events from chain
-cargo test --release --manifest-path $BASEDIR/../client/Cargo.toml -- --test-threads 1 --nocapture 
+run_tests() {
+    # release mode
+    # should find client/Cargo.toml no matter where its called from
+    # should run test in synchronous way to avoid race conditions
+    # output logs -- events from chain
+    sleep 1.5
+    cargo test --release --manifest-path $BASEDIR/../client/Cargo.toml -- --test-threads 1 --nocapture 
+}
+
+start_node() {
+   $BASEDIR/../scripts/run-standalone.sh > /dev/null 2>&1
+
+}
+
+start_node & run_tests
