5720e60b8d4fce6aa64d2d85765f8dab9bdd963e
Filip Lazovic <lazoviccorp@gmail.com>
2022-02-21 14:58:47 +0100

    Updeted script, attempt 2

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -3,18 +3,17 @@
 # get the path for this file
 BASEDIR=$(dirname "$0") 
 
+start_node() {
+   $BASEDIR/../scripts/run-standalone.sh > /dev/null 2>&1
+}
+
 run_tests() {
     # release mode
     # should find client/Cargo.toml no matter where its called from
     # should run test in synchronous way to avoid race conditions
     # output logs -- events from chain
-    sleep 1.5
-    cargo test --release --manifest-path $BASEDIR/../client/Cargo.toml -- --test-threads 1 --nocapture 
-}
-
-start_node() {
-   $BASEDIR/../scripts/run-standalone.sh > /dev/null 2>&1
-
+    sleep 2
+    cargo test --release -p webb-client -- --test-threads 1 --nocapture 
 }
 
 start_node & run_tests
