77e59f2c783cc4eb759d6f20a4e10c6b1ba49eb0
Filip Lazovic <lazoviccorp@gmail.com>
2022-03-15 17:59:04 +0100

    deps update

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -253,10 +253,10 @@ fn testnet_genesis(
 	let curve_bn254 = Curve::Bn254;
 	let curve_bls381 = Curve::Bls381;
 	log::info!("Bn254 x5 w3 params");
-	let bn254_x5_3_params = setup_params_x5_3::<ark_bn254::Fr>(curve_bn254);
+	let bn254_x5_3_params = setup_params::<ark_bn254::Fr>(curve_bn254, 5, 3);
 
 	log::info!("BLS381 x5 w3 params");
-	let bls381_x5_3_params = setup_params_x5_3::<ark_bls12_381::Fr>(curve_bls381);
+	let bls381_x5_3_params = setup_params::<ark_bls12_381::Fr>(curve_bls381, 5, 3);
 
 	log::info!("Verifier params");
 	let verifier_params = {
