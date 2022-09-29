1d9f293d9a6bf6470ec21fdae7b4e477370f39e6
Drew Stone <drewstone329@gmail.com>
2022-09-29 15:54:43 -0400

    Fix more tests

--- pallets/mt/src/lib.rs
+++ pallets/mt/src/lib.rs
@@ -339,7 +339,8 @@ pub mod pallet {
 		let default_zero = T::DefaultZeroElement::get();
 		temp_hashes.push(default_zero);
 		let mut temp_hash = default_zero.to_bytes().to_vec();
-		for _ in 0..T::MaxTreeDepth::get() {
+		for i in 0..T::MaxTreeDepth::get() {
+			println!("temp_hash: {:?}, {:?}", i, temp_hash);
 			temp_hash = T::Hasher::hash_two(&temp_hash, &temp_hash).unwrap();
 			temp_hashes.push(T::Element::from_vec(temp_hash.clone()));
 		}
