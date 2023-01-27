acc2b87270124c00ab470a20cd2d742c3ec88c5b
Kevin Huo <kevin.w.huo@gmail.com>
2023-01-27 11:03:07 -0500

    add reward pallet modifications

--- pallets/anonymity-mining/src/lib.rs
+++ pallets/anonymity-mining/src/lib.rs
@@ -30,20 +30,32 @@ pub mod mock;
 mod tests;
 
 use frame_support::{
+	dispatch::DispatchResultWithPostInfo,
+	ensure,
 	pallet_prelude::DispatchError,
 	sp_runtime::{
-		traits::{AccountIdConversion, Saturating},
+		traits::{AccountIdConversion, One, Saturating},
 		FixedI64, FixedPointNumber, SaturatedConversion,
 	},
 	traits::{Get, Time},
 	PalletId,
 };
 use orml_traits::MultiCurrency;
+pub use pallet::*;
 use pallet_vanchor::VAnchorConfigration;
 use sp_std::{convert::TryInto, vec};
-use webb_primitives::traits::vanchor::{VAnchorInspector, VAnchorInterface};
-
-pub use pallet::*;
+use webb_primitives::{
+	linkable_tree::{LinkableTreeInspector, LinkableTreeInterface},
+	traits::{
+		linkable_tree::*,
+		merkle_tree::*,
+		vanchor::{VAnchorInspector, VAnchorInterface},
+	},
+	types::vanchor::{ExtData, ProofData, VAnchorMetadata},
+	utils::compute_chain_id_type,
+	webb_proposals::{ResourceId, TargetSystem},
+	ElementTrait,
+};
 
 /// Type alias for the orml_traits::MultiCurrency::Balance type
 pub type BalanceOf<T, I> =
@@ -73,8 +85,22 @@ pub mod pallet {
 		type RuntimeEvent: From<Event<Self, I>>
 			+ IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
+		/// The overarching leaf index type
+		// type RootIndex: Encode + Decode + Parameter + AtLeast32Bit + Default + Copy +
+		// MaxEncodedLen;
+
 		/// Account Identifier from which the internal Pot is generated.
 		type PotId: Get<PalletId>;
+
+		// /// Pallet id
+		// #[pallet::constant]
+		// type PalletId: Get<PalletId>;
+
+		/// History size of roots for each deposit tree
+		type DepositRootHistorySize: Get<Self::RootIndex>;
+
+		/// History size of roots for each withdraw tree
+		type WithdrawRootHistorySize: Get<Self::RootIndex>;
 
 		/// Currency type for taking deposits
 		type Currency: MultiCurrency<Self::AccountId>;
@@ -116,6 +142,89 @@ pub mod pallet {
 		type ForceOrigin: EnsureOrigin<Self::RuntimeOrigin>;
 	}
 
+	/// TODO: keep track of last (30) deposit roots
+	/// Mapping from vanchor id to array of deposit roots and withdraw roots
+	/// ResourceId (webb rs) -> where proposal is stored, what data to execute etc.
+
+	/// Just need map of resource id to edge metadata
+	/// Similar to linkable tree but without chain id etc.
+
+	/// A helper map for denoting whether an tree is bridged to given chain
+	#[pallet::storage]
+	#[pallet::getter(fn linkable_tree_has_edge)]
+	pub type LinkableTreeHasEdge<T: Config<I>, I: 'static = ()> =
+		StorageMap<_, Blake2_128Concat, (T::TreeId, T::ChainId), bool, ValueQuery>;
+
+	/// The next deposit tree root index
+	#[pallet::storage]
+	#[pallet::getter(fn next_deposit_root_index)]
+	pub(super) type NextDepositRootIndex<T: Config<I>, I: 'static = ()> =
+		StorageValue<_, T::RootIndex, ValueQuery>;
+
+	/// The next withdraw tree root index
+	#[pallet::storage]
+	#[pallet::getter(fn next_withdraw_root_index)]
+	pub(super) type NextWithdrawRootIndex<T: Config<I>, I: 'static = ()> =
+		StorageValue<_, T::RootIndex, ValueQuery>;
+
+	// Map of treeId -> root index -> root value for deposit roots
+	#[pallet::storage]
+	#[pallet::getter(fn cached_deposit_roots)]
+	pub type CachedDepositRoots<T: Config<I>, I: 'static = ()> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		T::TreeId,
+		Blake2_128Concat,
+		T::RootIndex,
+		T::Element,
+		ValueQuery,
+	>;
+
+	// Map of treeId -> root index -> root value for withdraw roots
+	#[pallet::storage]
+	#[pallet::getter(fn cached_withdraw_roots)]
+	pub type CachedWithdrawRoots<T: Config<I>, I: 'static = ()> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		T::TreeId,
+		Blake2_128Concat,
+		T::RootIndex,
+		T::Element,
+		ValueQuery,
+	>;
+
+	// /// Map of resourceId to array of withdraw roots
+	// #[pallet::storage]
+	// #[pallet::getter(fn withdraw_roots)]
+	// pub type WithdrawRoots<T: Config<I>, I: 'static = ()> =
+	// 	StorageMap<_, Blake2_128Concat, T::TreeId, Vec<T::Element>, ValueQuery>;
+
+	/// The map of deposit trees to their spent nullifier hashes
+	#[pallet::storage]
+	#[pallet::getter(fn deposit_nullifier_hashes)]
+	pub type DepositNullifierHashes<T: Config<I>, I: 'static = ()> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		T::TreeId,
+		Blake2_128Concat,
+		T::Element,
+		bool,
+		ValueQuery,
+	>;
+
+	/// The map of withdraw trees to their spent nullifier hashes
+	#[pallet::storage]
+	#[pallet::getter(fn withdraw_nullifier_hashes)]
+	pub type WithdrawNullifierHashes<T: Config<I>, I: 'static = ()> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		T::TreeId,
+		Blake2_128Concat,
+		T::Element,
+		bool,
+		ValueQuery,
+	>;
+
 	#[pallet::genesis_config]
 	pub struct GenesisConfig<T: Config<I>, I: 'static = ()> {
 		pub phantom: (PhantomData<T>, PhantomData<I>),
@@ -146,12 +255,16 @@ pub mod pallet {
 	pub enum Event<T: Config<I>, I: 'static = ()> {
 		UpdatedPoolWeight { pool_weight: u64 },
 		UpdatedTokensSold { tokens_sold: u64 },
+		AnchorEdgeAdded,
+		AnchorEdgeUpdated,
 	}
 
 	#[pallet::error]
 	pub enum Error<T, I = ()> {
 		/// Error during hashing
 		HashError,
+		/// Invalid resource ID
+		InvalidResourceId,
 	}
 
 	#[pallet::call]
@@ -192,6 +305,171 @@ pub mod pallet {
 }
 
 impl<T: Config<I>, I: 'static> Pallet<T, I> {
+	// Add deposit nullifier hash
+	fn add_deposit_nullifier_hash(
+		id: T::TreeId,
+		nullifier_hash: T::Element,
+	) -> Result<(), DispatchError> {
+		DepositNullifierHashes::<T, I>::insert(id, nullifier_hash, true);
+		Ok(())
+	}
+
+	// Add withdraw nullifier hash
+	fn add_withdraw_nullifier_hash(
+		id: T::TreeId,
+		nullifier_hash: T::Element,
+	) -> Result<(), DispatchError> {
+		WithdrawNullifierHashes::<T, I>::insert(id, nullifier_hash, true);
+		Ok(())
+	}
+
+	/// Store nullifier hashes in deposit and withdraw reward trees
+	fn store_nullifier_hashes(
+		id: T::TreeId,
+		deposit_nullifier_hash: T::Element,
+		withdraw_nullifier_hash: T::Element,
+	) -> Result<(), DispatchError> {
+		Self::add_deposit_nullifier_hash(id, deposit_nullifier_hash)?;
+		Self::add_withdraw_nullifier_hash(id, withdraw_nullifier_hash)?;
+		Ok(())
+	}
+
+	pub fn handle_proof_verification(
+		proof_data: &ProofData<T::Element>,
+	) -> Result<(), DispatchError> {
+		// Stubbed out for now
+		Ok(())
+	}
+
+	/// Update vanchor
+	fn update_vanchor(
+		tree_id: T::TreeId,
+		merkle_root: T::Element,
+		src_resource_id: ResourceId,
+		latest_leaf_index: T::LeafIndex,
+	) -> DispatchResultWithPostInfo {
+		let src_chain_id: T::ChainId = src_resource_id.typed_chain_id().chain_id().into();
+		if T::VAnchor::has_edge(tree_id, src_chain_id) {
+			T::VAnchor::update_edge(
+				tree_id,
+				src_chain_id,
+				merkle_root,
+				latest_leaf_index,
+				src_resource_id,
+			)?;
+			Self::deposit_event(Event::AnchorEdgeUpdated);
+		} else {
+			T::VAnchor::add_edge(
+				tree_id,
+				src_chain_id,
+				merkle_root,
+				latest_leaf_index,
+				src_resource_id,
+			)?;
+			Self::deposit_event(Event::AnchorEdgeAdded);
+		}
+		Ok(().into())
+	}
+
+	fn update_roots(
+		src_tree_id: T::TreeId,
+		dest_tree_id: T::TreeId,
+		src_root_index: T::RootIndex,
+		dest_root_index: T::RootIndex,
+		src_deposit_root: T::Element,
+		src_withdraw_root: T::Element,
+		dest_deposit_root: T::Element,
+		dest_withdraw_root: T::Element,
+	) {
+		// Update roots
+		CachedDepositRoots::<T, I>::insert(src_tree_id, src_root_index, src_deposit_root);
+		CachedWithdrawRoots::<T, I>::insert(src_tree_id, src_root_index, src_withdraw_root);
+		CachedDepositRoots::<T, I>::insert(dest_tree_id, dest_root_index, dest_deposit_root);
+		CachedWithdrawRoots::<T, I>::insert(dest_tree_id, dest_root_index, dest_withdraw_root);
+	}
+
+	/// Claim AP tokens and update rewards VAnchor
+	pub fn claim_ap(
+		src_resource_id: ResourceId,
+		dest_resource_id: ResourceId,
+		recipient: T::AccountId,
+		amount: BalanceOf<T, I>,
+		merkle_root: T::Element,
+		latest_leaf_index: T::LeafIndex,
+		proof_data: ProofData<T::Element>,
+		src_deposit_root: T::Element,
+		src_withdraw_root: T::Element,
+		dest_deposit_root: T::Element,
+		dest_withdraw_root: T::Element,
+	) -> DispatchResultWithPostInfo {
+		// Transfer AP tokens to recipient
+		<T as Config<I>>::Currency::transfer(
+			T::AnonymityPointsAssetId::get(),
+			&Self::account_id(),
+			&recipient,
+			amount,
+		)?;
+
+		let source_tree_id: T::TreeId = match src_resource_id.target_system() {
+			TargetSystem::Substrate(system) => system.tree_id.into(),
+			_ => {
+				ensure!(false, Error::<T, I>::InvalidResourceId);
+				T::TreeId::default()
+			},
+		};
+
+		let dest_tree_id: T::TreeId = match dest_resource_id.target_system() {
+			TargetSystem::Substrate(system) => system.tree_id.into(),
+			_ => {
+				ensure!(false, Error::<T, I>::InvalidResourceId);
+				T::TreeId::default()
+			},
+		};
+
+		let deposit_root_index = Self::next_deposit_root_index();
+		let withdraw_root_index = Self::next_withdraw_root_index();
+
+		NextDepositRootIndex::<T, I>::mutate(|i| {
+			*i = i.saturating_add(One::one()) % T::DepositRootHistorySize::get()
+		});
+		NextWithdrawRootIndex::<T, I>::mutate(|i| {
+			*i = i.saturating_add(One::one()) % T::DepositRootHistorySize::get()
+		});
+
+		// Update roots - TODO: modify
+		Self::update_roots(
+			source_tree_id,
+			dest_tree_id,
+			deposit_root_index,
+			withdraw_root_index,
+			src_deposit_root,
+			src_withdraw_root,
+			dest_deposit_root,
+			dest_withdraw_root,
+		);
+
+		// Update rewards VAnchor
+		let vanchor =
+			Self::update_vanchor(source_tree_id, merkle_root, src_resource_id, latest_leaf_index)?;
+
+		// Handle proof verification
+		Self::handle_proof_verification(&proof_data)?;
+
+		// Flag deposit nullifier as being used, TODO: modify
+		for nullifier in &proof_data.input_nullifiers {
+			Self::add_deposit_nullifier_hash(source_tree_id, *nullifier)?;
+		}
+
+		// Insert output commitments into the AP tree
+		for comm in &proof_data.output_commitments {
+			T::LinkableTree::insert_in_order(source_tree_id, *comm)?;
+		}
+
+		// TODO: event?
+
+		Ok(().into())
+	}
+
 	/// Get a unique, inaccessible account id from the `PotId`.
 	pub fn account_id() -> T::AccountId {
 		T::PotId::get().into_account_truncating()
--- pallets/anonymity-mining/src/tests.rs
+++ pallets/anonymity-mining/src/tests.rs
@@ -1,10 +1,22 @@
 use super::*;
 use crate::mock::*;
+use std::default::Default;
 
 use frame_benchmarking::account;
 use frame_support::assert_ok;
 
 use sp_runtime::traits::Zero;
+
+use webb_primitives::{
+	types::vanchor::ProofData,
+	webb_proposals::{
+		FunctionSignature, ResourceId, SubstrateTargetSystem, TargetSystem, TypedChainId,
+	},
+};
+
+use arkworks_setups::{common::setup_params, Curve};
+
+use pallet_vanchor_handler::AnchorList;
 
 const SEED: u32 = 0;
 const START_TIMESTAMP: u64 = 0;
@@ -12,6 +24,9 @@ const INITIAL_LIQUIDITY: u128 = 10000000;
 const LIQUIDITY: u128 = 20000000;
 const INITIAL_TOTAL_REWARDS_BALANCE: i128 = 30000000;
 const DURATION: u64 = 31536000;
+
+const TEST_MAX_EDGES: u32 = 100;
+const TEST_TREE_DEPTH: u8 = 32;
 
 #[test]
 fn should_initialize_parameters() {
@@ -29,6 +44,117 @@ fn setup_environment() {
 		assert_ok!(Balances::set_balance(RuntimeOrigin::root(), account_id, 100_000_000, 0));
 	}
 }
+
+// helper function to create anchor using Anchor pallet call
+fn mock_vanchor_creation_using_pallet_call(resource_id: &ResourceId) {
+	assert!(!<pallet_mt::Trees<Test>>::contains_key(0));
+	assert_ok!(VAnchor::create(RuntimeOrigin::root(), TEST_MAX_EDGES, TEST_TREE_DEPTH, 0));
+	AnchorList::<Test>::insert(resource_id, 0);
+	assert!(<pallet_mt::Trees<Test>>::contains_key(0));
+	assert_eq!(TEST_MAX_EDGES, <pallet_linkable_tree::MaxEdges<Test>>::get(0));
+}
+
+/// AP claim tests
+
+// Test claim_ap
+#[test]
+fn test_claim_ap() {
+	new_test_ext().execute_with(|| {
+		setup_environment();
+
+		let recipient_one_account_id = account::<AccountId>("", 2, SEED);
+		let sender_two_account_id = account::<AccountId>("", 3, SEED);
+
+		let src_id = TypedChainId::Substrate(1);
+		let target_id = TypedChainId::Substrate(5);
+		let target_system =
+			TargetSystem::Substrate(SubstrateTargetSystem { pallet_index: 11, tree_id: 0 });
+		let r_id: ResourceId = ResourceId::new(target_system, target_id);
+
+		let root = Element::from_bytes(&[1; 32]);
+		let latest_leaf_index = 5;
+		let src_target_system = target_system;
+		let src_resource_id = ResourceId::new(src_target_system, src_id);
+
+		let dest_target_system = target_system;
+		let dest_resource_id = ResourceId::new(dest_target_system, target_id);
+
+		// print out r_id
+		println!("r_id: {:?}", r_id);
+
+		let tree_id = 5;
+
+		// token setup
+		let ap_currency_id = 1;
+		let reward_currency_id = 2;
+
+		// add reward balance to pallet
+		let new_reward_balance = INITIAL_TOTAL_REWARDS_BALANCE;
+		assert_ok!(Currencies::update_balance(
+			RuntimeOrigin::root(),
+			AnonymityMining::account_id(),
+			reward_currency_id,
+			new_reward_balance,
+		));
+
+		// adding AP balance to pallet
+		let new_ap_balance = 50000;
+		assert_ok!(Currencies::update_balance(
+			RuntimeOrigin::root(),
+			AnonymityMining::account_id(),
+			ap_currency_id,
+			new_ap_balance,
+		));
+
+		// param setup
+		let curve = Curve::Bn254;
+		let params = setup_params::<ark_bn254::Fr>(curve, 5, 3);
+		let _ = HasherPallet::force_set_parameters(
+			RuntimeOrigin::root(),
+			params.to_bytes().try_into().unwrap(),
+		);
+
+		SignatureBridge::whitelist_chain(RuntimeOrigin::root(), src_id.chain_id());
+		SignatureBridge::set_resource(RuntimeOrigin::root(), r_id);
+		SignatureBridge::resource_exists(r_id);
+
+		mock_vanchor_creation_using_pallet_call(&r_id);
+
+		// mock proof data
+		let proof_data = ProofData {
+			proof: vec![],
+			public_amount: Default::default(),
+			roots: vec![],
+			input_nullifiers: vec![],
+			output_commitments: vec![],
+			ext_data_hash: Default::default(),
+		};
+
+		// mock roots
+		let src_deposit_root = Default::default();
+		let src_withdraw_root = Default::default();
+		let dest_deposit_root = Default::default();
+		let dest_withdraw_root = Default::default();
+
+		let claim_ap_call = AnonymityMining::claim_ap(
+			src_resource_id,
+			dest_resource_id,
+			recipient_one_account_id,
+			1000,
+			root,
+			latest_leaf_index,
+			proof_data,
+			src_deposit_root,
+			src_withdraw_root,
+			dest_deposit_root,
+			dest_withdraw_root,
+		);
+
+		assert_ok!(claim_ap_call);
+	})
+}
+
+/// Rewards swapping tests
 
 // Test basic set pool weight
 #[test]
