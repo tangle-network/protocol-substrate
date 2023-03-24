95df73bf24fcfaf45c76cb8000da27eed1b91d37
Felix Ableitner <me@nutomic.com>
2023-03-24 16:41:00 +0100

    Change token decimals to 18

--- standalone/runtime/src/constants.rs
+++ standalone/runtime/src/constants.rs
@@ -19,7 +19,7 @@ pub mod currency {
 	/// The existential deposit. Set to 1/10 of its parent Relay Chain (v9020).
 	pub const EXISTENTIAL_DEPOSIT: Balance = CENTS / 10;
 
-	pub const UNITS: Balance = 1_000_000_000_000;
+	pub const UNITS: Balance = 1_000_000_000_000_000_000;
 	pub const KUNITS: Balance = UNITS * 1000;
 	pub const DOLLARS: Balance = UNITS;
 	pub const CENTS: Balance = UNITS / 30_000;
